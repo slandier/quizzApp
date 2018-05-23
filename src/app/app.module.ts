18bc587f4d91fbd03103986e054b165ed0613d93
sara <sara.landier@gmail.com>
2018-05-23 14:57:46 +0200

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { OptionsPage } from '../pages/options/options';
+import { QuestionPage } from '../pages/question/question';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    OptionsPage,
+    QuestionPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    OptionsPage,
+    QuestionPage
   ],
   providers: [
     StatusBar,
